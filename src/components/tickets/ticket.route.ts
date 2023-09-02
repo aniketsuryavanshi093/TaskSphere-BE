656803383abdf5749af2ff59a43de8b2b9e9eab9
M-Ubaid-Asif <ubaid6664@gmail.com>
2023-09-03 00:07:09 +0530

    created get all ticket routes

--- src/components/tickets/ticket.route.ts
+++ src/components/tickets/ticket.route.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import { userAuth } from '@middlewares/auth'
-import { createTicket } from './ticket.cotroller'
+import { createTicket, getAllTicket } from './ticket.cotroller'
 import { checkIsTicketAdministrator } from '@middlewares/checkIsTicketAdministrator'
 const router = express.Router()
 
-router.use(userAuth)
+// router.use(userAuth)
 router.post('/create', checkIsTicketAdministrator, createTicket)
+router.get('/allTickets', getAllTicket)
 
 export default router
