f29c0aaa3dffb7518370580645b0cd9f6026dddf
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2023-10-25 21:43:05 +0530

    blog api implemented

--- src/components/blog/controller.ts
+++ src/components/blog/controller.ts
@@ -1,6 +1,6 @@
-import { NextFunction } from "express"
-import { createBlogService } from "./service"
-import { handleResponse } from "@helpers/errorHandler"
+import { NextFunction } from 'express'
+import { createBlogService, getAblogService, getAllblogsService } from './service'
+import { handleResponse } from '@helpers/errorHandler'
 
 export const createBlog = async (
     req: Request,
@@ -13,6 +13,45 @@ export const createBlog = async (
         return handleResponse({
             res,
             data: { ...data._doc },
+        })
+    } catch (error: any) {
+        if (error.isJoi === true) {
+            error.statusCode = 422
+        }
+        next(error)
+    }
+}
+
+export const getAllblogs = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void | Response> => {
+    try {
+        const data = await getAllblogsService()
+        return handleResponse({
+            res,
+            data,
+        })
+    } catch (error: any) {
+        if (error.isJoi === true) {
+            error.statusCode = 422
+        }
+        next(error)
+    }
+}
+
+export const getBlog = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+): Promise<void | Response> => {
+    try {
+        const { slug } = req.params
+        const data = await getAblogService(slug)
+        return handleResponse({
+            res,
+            data,
         })
     } catch (error: any) {
         if (error.isJoi === true) {
--- src/components/blog/route.ts
+++ src/components/blog/route.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { userAuth } from '@middlewares/auth'
-import { createBlog } from './controller'
+import { createBlog, getAllblogs, getBlog } from './controller'
 
 const router = express.Router()
 
 router.post('/createBlog', userAuth, createBlog)
+router.get('/getallblogs', getAllblogs)
+router.get('/getblog/:slug', getBlog)
 export default router
